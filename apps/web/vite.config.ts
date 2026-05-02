52eb0543afcca9bb61b14cf48ffa82570b9f0c97
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2026-05-02 12:50:52 +0200

    chore: updated vite.config.ts and .gitignore files

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@cro/shared': path.resolve(__dirname, '../../packages/shared/src'),
+    },
+  },
   server: {
     port: 5173,
     host: true,
   },
-  optimizeDeps: {
-    include: ['@cro/shared'],
-  },
-})
+});
